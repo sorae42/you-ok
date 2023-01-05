90844fcf332c39f1503f9b5432ee219f20a0f887
Sora <34794115+sorae42@users.noreply.github.com>
2023-01-05 13:38:51 +0700

    fix buddy page again

--- src/Layout/Buddy.svelte
+++ src/Layout/Buddy.svelte
@@ -1,18 +1,6 @@
-<script lang="ts">
-    import Notice from './Notice.svelte';
-</script>
-
 <svelte:head>
     <title>Coming Soon! - YouOkay</title>
 </svelte:head>
 
-<Notice
-    icon="triangle-exclamation"
-    title="Buddy List is in the work!"
-    content="The core function is very buggy. Some stuff might not work or even break."
-/>
-<Notice
-    icon="triangle-exclamation"
-    title="Buddy List is not secure!"
-    content="Security protocol for the buddy system is currently turned off for development."
-/>
+<h2>A work in progress!</h2>
+<p>Buddy system is on the work!</p>
